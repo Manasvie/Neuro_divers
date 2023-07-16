855cf6fc095466a025ef67007845d38552c6d2d2
Manasvi Bagherwal <118369715+Manasvie@users.noreply.github.com>
2023-07-16 18:05:02 +0530

    Add files via upload

---
+++ src/Styles/form.jsx
@@ -0,0 +1,126 @@
+// import React, { useState, useEffect } from 'react';
+// // import axios from 'axios';
+// import './Form.css';
+
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   const handleSubmit = event => {
+//    event.preventDefault();
+//    alert('You have submitted the form.')
+//  }
+
+//   return(
+//     <div className="wrapper">
+//       <h1>How About Them Apples</h1>
+//       <form onSubmit={handleSubmit}>
+//         <fieldset>
+//           <label>
+//             <p>Name</p>
+//             <input name="name" />
+//           </label>
+//         </fieldset>
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// function Form() {
+//   const [name, setName] = useState('');
+//   const [college, setCollege] = useState('');
+//   const [students, setStudents] = useState([]);
+
+//   useEffect(() => {
+//     fetchStudents();
+//   }, []);
+
+//   const fetchStudents = async () => {
+//     try {
+//       const response = await axios.get('http://localhost:5000/students');
+//       setStudents(response.data);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+  
+//     if (name === '' || college === '') {
+//       alert('Please fill in all fields');
+//       return;
+//     }
+  
+//     const formData = new FormData();
+//     formData.append('name', name);
+//     formData.append('college', college);
+  
+//     try {
+//       await axios.post('http://localhost:5000/students', formData, {
+//         headers: {
+//           'Content-Type': 'multipart/form-data',
+//         },
+//       });
+//       fetchStudents();
+//       setName('');
+//       setCollege('');
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+  
+
+// return (
+//     <div className="form-container">
+//       <h1 className='heading'>Student Data</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div className="form-group">
+//           <label htmlFor="name">Name:</label>
+//           <input
+//             type="text"
+//             id="name"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="college">College:</label>
+//           <input
+//             type="text"
+//             id="college"
+//             value={college}
+//             onChange={(e) => setCollege(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <button type="submit">Submit</button>
+//       </form>
+//       <h2 className='heading'>Submitted Data:</h2>
+//       <div className="submitted-data-container">
+//       <table className="submitted-data">
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>College</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {students.map((student, index) => (
+//             <tr key={index}>
+//               <td>{student.name}</td>
+//               <td>{student.college}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//      </div>
+//     </div>
+//   );
+// }
+
+// export default Form;
